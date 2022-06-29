f95e086f4cb75b2d0283c063468032ed0897b0bf
IKyllian <kyllian.delporte@gmail.com>
2022-06-29 13:35:31 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import Image1 from "./Images/desktop-image-hero-1.jpg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container-room'>
+      <div className='grid-room'>
+        <div>
+          <img className='img' src={Image1} alt="first img"></img>
+        </div>
+
+        <div>
+          <div>
+            <h1> Discover innovative ways to decorate</h1>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. In egestas faucibus diam, et consequat orci semper non. Pellentesque efficitur felis a cursus porttitor. Cras lacinia at orci a euismod. Aenean elementum commodo lacinia. Nullam egestas a lorem quis molestie. Donec eget tincidunt tortor. Aenean eu leo non metus varius tempor.</p>
+            <p>SHOP NOW</p>
+          </div>
+         
+        </div>
+
+        <div>
+          
+        </div>
+
+        <div>
+          
+        </div>
+
+        <div>
+          
+        </div>
+
+      </div>
     </div>
   );
 }
