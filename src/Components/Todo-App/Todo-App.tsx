9dea2a28610505ff474389f18f692dc70ef84455
IKyllian <kyllian.delporte@gmail.com>
2022-07-05 11:03:43 +0200

    added functionality to delete a todo item

--- src/Components/Todo-App/Todo-App.tsx
+++ src/Components/Todo-App/Todo-App.tsx
@@ -25,7 +25,7 @@ function TodoApp() {
     }
 
     const handleActive: Function = (index: number): void => {
-        let newArray  = [...todos];
+        let newArray: TodoDatas[]  = [...todos];
 
         if (todos[index].isActive === true)
             newArray[index].isActive = false;
@@ -35,7 +35,7 @@ function TodoApp() {
     }
 
     const handleComplete: Function = (index: number): void => {
-        let newArray  = [...todos];
+        let newArray: TodoDatas[] = [...todos];
 
         if (todos[index].isComplete === true)
             newArray[index].isComplete = false;
@@ -45,14 +45,7 @@ function TodoApp() {
     }
 
     const itemsLeft = (): number => {
-        let nb = 0;
-        if (todos.length <= 0)
-            return 0;
-        todos.map((element):void => {
-            if (element.isComplete == false)
-                nb++;
-        })
-        return nb;
+        return todos.filter((element: TodoDatas) => element.isComplete == false).length;
     }
 
     const removeCompletedElements = (): void => {
@@ -63,16 +56,22 @@ function TodoApp() {
         if (filter == "all") {
             return(
                 todos.map((element: TodoDatas, index: number) =>
-                    <TodoItem key={index} element={element} index={index} handleActive={handleActive} handleComplete={handleComplete} theme={theme} />
+                    <TodoItem key={index} element={element} index={index} handleActive={handleActive} handleComplete={handleComplete} theme={theme} removeItem={removeItem} />
                 )
             )
         } else {
             return (
                 todos.filter((element: TodoDatas) => filter === "active" ? element.isActive === true : element.isComplete === true).map((element: TodoDatas, index) =>
-                    <TodoItem key={index} element={element} index={index} handleActive={handleActive} handleComplete={handleComplete} theme={theme} />
+                    <TodoItem key={index} element={element} index={index} handleActive={handleActive} handleComplete={handleComplete} theme={theme} removeItem={removeItem} />
                 )
             )
         }
+    }
+
+    const removeItem = (index: number): void => {
+        let newArray: TodoDatas[] = [...todos];
+        newArray.splice(index, 1);
+        setTodos(newArray);
     }
     
     return (
@@ -83,8 +82,8 @@ function TodoApp() {
                     <h1> todo </h1>
                     <div onClick={() => theme == "dark" ? setTheme("light") : setTheme("dark")}>
                         {
-                            theme == "dark" ? <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26"><path fill="#FFF" fill-rule="evenodd" d="M13 21a1 1 0 011 1v3a1 1 0 11-2 0v-3a1 1 0 011-1zm-5.657-2.343a1 1 0 010 1.414l-2.121 2.121a1 1 0 01-1.414-1.414l2.12-2.121a1 1 0 011.415 0zm12.728 0l2.121 2.121a1 1 0 01-1.414 1.414l-2.121-2.12a1 1 0 011.414-1.415zM13 8a5 5 0 110 10 5 5 0 010-10zm12 4a1 1 0 110 2h-3a1 1 0 110-2h3zM4 12a1 1 0 110 2H1a1 1 0 110-2h3zm18.192-8.192a1 1 0 010 1.414l-2.12 2.121a1 1 0 01-1.415-1.414l2.121-2.121a1 1 0 011.414 0zm-16.97 0l2.121 2.12A1 1 0 015.93 7.344L3.808 5.222a1 1 0 011.414-1.414zM13 0a1 1 0 011 1v3a1 1 0 11-2 0V1a1 1 0 011-1z"/></svg>
-                            : <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26"><path fill="#FFF" fill-rule="evenodd" d="M13 0c.81 0 1.603.074 2.373.216C10.593 1.199 7 5.43 7 10.5 7 16.299 11.701 21 17.5 21c2.996 0 5.7-1.255 7.613-3.268C23.22 22.572 18.51 26 13 26 5.82 26 0 20.18 0 13S5.82 0 13 0z"/></svg>
+                            theme == "dark" ? <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26"><path fill="#FFF" fillRule="evenodd" d="M13 21a1 1 0 011 1v3a1 1 0 11-2 0v-3a1 1 0 011-1zm-5.657-2.343a1 1 0 010 1.414l-2.121 2.121a1 1 0 01-1.414-1.414l2.12-2.121a1 1 0 011.415 0zm12.728 0l2.121 2.121a1 1 0 01-1.414 1.414l-2.121-2.12a1 1 0 011.414-1.415zM13 8a5 5 0 110 10 5 5 0 010-10zm12 4a1 1 0 110 2h-3a1 1 0 110-2h3zM4 12a1 1 0 110 2H1a1 1 0 110-2h3zm18.192-8.192a1 1 0 010 1.414l-2.12 2.121a1 1 0 01-1.415-1.414l2.121-2.121a1 1 0 011.414 0zm-16.97 0l2.121 2.12A1 1 0 015.93 7.344L3.808 5.222a1 1 0 011.414-1.414zM13 0a1 1 0 011 1v3a1 1 0 11-2 0V1a1 1 0 011-1z"/></svg>
+                            : <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26"><path fill="#FFF" fillRule="evenodd" d="M13 0c.81 0 1.603.074 2.373.216C10.593 1.199 7 5.43 7 10.5 7 16.299 11.701 21 17.5 21c2.996 0 5.7-1.255 7.613-3.268C23.22 22.572 18.51 26 13 26 5.82 26 0 20.18 0 13S5.82 0 13 0z"/></svg>
                         }
                     </div>
                 </div>
--- src/Components/Todo-App/TodoItem.tsx
+++ src/Components/Todo-App/TodoItem.tsx
@@ -12,9 +12,10 @@ type Props = {
     handleActive: Function,
     handleComplete: Function,
     theme: string,
+    removeItem: Function,
 }
 
-const TodoItem: React.FC<Props> = ({element, index, handleActive, handleComplete, theme}) => {
+const TodoItem: React.FC<Props> = ({element, index, handleActive, handleComplete, theme, removeItem}) => {
     return (
         <li className={`li-${theme}`}>
             <div className={`circle-check ${element.isActive === true ? "circle-check-active" : ""}`} onClick={() => handleActive(index)}>
@@ -24,6 +25,7 @@ const TodoItem: React.FC<Props> = ({element, index, handleActive, handleComplete
                 }
             </div>
         <p className={`${element.isComplete === true ? "todo-completed" : ""}`} onClick={() => handleComplete(index)}> {element.content} </p>
+        <svg className="delete-item" onClick={() => removeItem(index)} xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fillRule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>
     </li>
     );
 }
